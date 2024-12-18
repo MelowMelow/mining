29219b8c78c8f9a28bf7631d8e202a955a38a1f0
MelowMelow <130892329+MelowMelow@users.noreply.github.com>
2024-12-18 13:32:23 +0700

    Add files via upload

--- api/updateResources.js
+++ api/updateResources.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client
-const supabase = createClient('SUPABASE_URL', 'SUPABASE_KEY');
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
