d08d917a84fba8de73f013632c10bb11e2a84044
MelowMelow <130892329+MelowMelow@users.noreply.github.com>
2024-12-21 23:15:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.getElementById("leaderboard-button").addEventListener("click", toggleLe
 
 
 // Start mining process when the user clicks the mine button
-function startMining() {
+async function startMining() {
   
   
  
