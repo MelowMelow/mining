6a8b9d385088a1a34e98e061da04cdcf0a54b2df
MelowMelow <130892329+MelowMelow@users.noreply.github.com>
2024-12-22 01:53:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+import { handleAuthentication } from './login.js';
 
 let energy = 1000;
 let isMining = false;
@@ -19,9 +19,9 @@ document.getElementById("leaderboard-button").addEventListener("click", toggleLe
 
 
 // Start mining process when the user clicks the mine button
-function startMining() {
+async function startMining() {
   console.log("Mining button clicked!");
-
+  await handleAuthentication();
      
   if (isMining || energy < 30) {
     console.log("Not enough energy to mine.");
