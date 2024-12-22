2eea5e2e5a29b869d547f2ca927361250c7fdae4
MelowMelow <130892329+MelowMelow@users.noreply.github.com>
2024-12-22 18:18:33 +0700

    Add files via upload

--- api/updateResources.js
+++ api/updateResources.js
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Initialize Supabase client with environment variables
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
