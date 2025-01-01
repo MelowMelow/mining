c9c252ba67f02ac5b7b94f28b4c6259e0f9ff963
MelowMelow <130892329+MelowMelow@users.noreply.github.com>
2025-01-02 00:48:58 +0700

    Update script.jsx

--- src/script.jsx
+++ src/script.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import UpgradeMenu from './UpgradeMenu.jsx';
+import UpgradeMenu from './components/UpgradeMenu.jsx';
 
 
 
